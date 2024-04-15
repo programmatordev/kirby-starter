380d2343d442665ed7f0cf8b6f3f7372d312be5f
André Pimpão <a.pimpao@programmator.dev>
2024-04-15 11:43:02 +0100

    chore: hide robots settings and indicator from users that are not admin

--- site/config/config.php
+++ site/config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 use Beebmx\KirbyEnv;
+use Kirby\Cms\App as Kirby;
 
 KirbyEnv::load('.');
 
@@ -12,5 +13,14 @@ return [
     ],
     'auth' => [
         'methods' => ['password', 'password-reset']
-    ]
+    ],
+    'ready' => function(Kirby $kirby) {
+        $user = $kirby->user();
+
+        return [
+            // show robots settings to admins only
+            'tobimori.seo.robots.pageSettings' => $user?->isAdmin() ?? false,
+            'tobimori.seo.robots.indicator' => $user?->isAdmin() ?? false
+        ];
+    }
 ];
