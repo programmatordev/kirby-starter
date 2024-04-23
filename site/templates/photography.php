dfd3780106201bc2d57e36a82a449b18482e1c28
André Pimpão <a.pimpao@programmator.dev>
2024-04-23 09:47:54 +0100

    chore: added missing photography page

---
+++ site/templates/photography.php
@@ -0,0 +1,35 @@
+<?php
+/** @var \Kirby\Cms\Site $site */
+/** @var \Kirby\Cms\Page $page */
+?>
+
+<?php snippet('layout', slots: true) ?>
+
+<?php slot('content') ?>
+  <?php snippet('intro') ?>
+
+  <ul class="grid grid-cols-1 gap-6 md:grid-cols-12">
+    <?php foreach ($page->children()->listed() as $album): ?>
+      <li class="mb-6 md:col-span-3">
+        <a href="<?= $album->url() ?>">
+          <figure>
+            <span class="relative block bg-black aspect-[4/5]">
+              <?php /** @var \Kirby\Cms\File $cover */ ?>
+              <?php if ($cover = $album->cover()->toFile()): ?>
+                <img
+                  src="<?= $cover->crop(400, 500)->url() ?>"
+                  alt="<?= $cover->alt()->esc() ?>"
+                  class="absolute inset-0 w-full h-full border-0 object-cover"
+                >
+              <?php endif ?>
+            </span>
+
+            <figcaption class="pt-3 leading-6">
+              <?= $album->title()->esc() ?>
+            </figcaption>
+          </figure>
+        </a>
+      </li>
+    <?php endforeach ?>
+  </ul>
+<?php endslot() ?>
