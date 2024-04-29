1967c63da41750eb041145a3b70b51db5cf4bc30
André Pimpão <a.pimpao@programmator.dev>
2024-04-29 16:07:46 +0100

    chore: added site seo

--- site/config/config.php
+++ site/config/config.php
@@ -9,9 +9,6 @@ KirbyEnv::load('.');
 return [
     'debug' => env('KIRBY_DEBUG', true),
     'languages' => true,
-    'panel' => [
-        'language' => 'en'
-    ],
     'auth' => [
         'methods' => ['password', 'password-reset']
     ],
