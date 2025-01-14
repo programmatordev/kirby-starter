536ee9027f1c068cb722605d957622a8ef69e96a
André Pimpão <a.pimpao@programmator.dev>
2025-01-14 18:57:47 +0000

    feat: add alpine.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,14 @@
 import { Core } from "@unseenco/taxi";
 import DefaultRenderer from "./renderers/default.js";
 import FadeTransition from "./transitions/fade.js";
+import { Alpine } from "alpinejs";
 
 // import tailwind file
 import "../css/app.css";
+
+// init reactivity
+window.Alpine = Alpine;
+Alpine.start();
 
 // handle page rendering and transitions
 const taxi = new Core({
