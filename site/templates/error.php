8009b0ac97d41255706cae3d226dd0c00e1052fe
André Pimpão <a.pimpao@programmator.dev>
2026-01-02 17:01:44 +0000

    chore: add data-taxi-view to error page

--- site/templates/error.php
+++ site/templates/error.php
@@ -7,6 +7,10 @@
 <?php snippet('layout', slots: true) ?>
 
 <?php slot('content') ?>
+
+<div data-taxi-view>
   <h1 class="font-bold"><?= $page->title() ?></h1>
   <div><?= $page->text()->kirbytext() ?></div>
+</div>
+
 <?php endslot(); ?>
