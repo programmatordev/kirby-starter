d8def141ab6a71b3ed586b47694c3bf2e1717c48
André Pimpão <a.pimpao@programmator.dev>
2026-01-05 18:47:38 +0000

    chore: move alpinejs to its own plugin and improve state handling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,13 @@
 import { Core } from "@unseenco/taxi";
 import DefaultRenderer from "./renderers/DefaultRenderer.js";
 import FadeTransition from "./transitions/FadeTransition.js";
-import { Alpine } from "alpinejs";
 
 // import the tailwind file
 import "../styles/app.css";
 
-// add Alpine global and init reactivity
-window.Alpine = Alpine;
-Alpine.start();
-
 // handle pages rendering and transitions
-const taxi = new Core({
+// save for global access
+window.Taxi = new Core({
   renderers: { default: DefaultRenderer },
   transitions: { default: FadeTransition }
 });
